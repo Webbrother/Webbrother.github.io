e2e7e0231fb64b54edd74f78e92c58d7efb20d6f
Vitaly K <komarov.vitaliy@gmail.com>
2019-02-25 19:42:25 +0400

    Updates

---
+++ precache-manifest.a0af5f29e3f71cf761eb9e348991d050.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18eb8f99b20a4c0303b1",
+    "url": "/static/css/main.f5562e31.chunk.css"
+  },
+  {
+    "revision": "18eb8f99b20a4c0303b1",
+    "url": "/static/js/main.18eb8f99.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "c4c3d392f74ce2cfe87d6f387d4417df",
+    "url": "/index.html"
+  }
+];
